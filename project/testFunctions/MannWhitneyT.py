8ea2dfb3d9e221400b2490f81b1c415c7d24d4b1
axel <mboumeu581@gmail.com>
2024-04-24 08:16:39 +0100

    achived man whitney

---
+++ project/testFunctions/MannWhitneyT.py
@@ -0,0 +1,22 @@
+import sys
+import os
+
+# Add the parent directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Now you can import the module from the parent package
+from tests.MannWhitney import MannWhitney
+
+
+if __name__ == '__main__':
+    data1 = [5, 7, 3, 8, 6]
+    data2 = [10, 12, 9, 11, 13]
+    mw = MannWhitney(data1, data2)
+    mw.steps()
+
+    data1 = [2.1, 4.0, 6.3, 5.4, 4.8,3.7,6.1,3.3]
+    data2 = [4.1, 0.6, 3.1, 2.5, 4.0 ,6.2 ,1.6 ,2.2 ,1.9, 5.4]
+    mw = MannWhitney(data2, data1)
+    mw.steps()
+
+
--- project/tests/MannWhitney.py
+++ project/tests/MannWhitney.py
@@ -1,20 +1,54 @@
 from scipy.stats import mannwhitneyu
 
+class MannWhitney:
+    def __init__(self, data1, data2,type="two-sided"):
+        self.data1 = data1
+        self.data2 = data2
+    
+    def formHyp(self):
+        
+        
+        return "H_0: U_A = U_B \n H_1: U_A = U_B"
+    
+    def distribution(self):
+        # Add your Mann-Whitney U test p-value calculation here
 
-# Example data for two independent samples
-sample1 = [85, 92, 78, 89, 91]
-sample2 = [69, 74, 82, 68, 73]
+       pass
+    
 
-# Perform Mann-Whitney U test
-statistic, p_value = mannwhitneyu(sample1, sample2)
-print ("I'm here")
-# Output the results
-print("Mann-Whitney U statistic:", statistic)
-print("p-value:", p_value)
+    def testval(self):
+        
+        result = mannwhitneyu(self.data1, self.data2)
+        return result
+    
 
-# Interpret the results
-alpha = 0.05
-if p_value < alpha:
-    print("Reject the null hypothesis: There is a significant difference between the samples.")
-else:
-    print("Fail to reject the null hypothesis: There is no significant difference between the samples.")
+    
+    def steps(self):
+
+        result=self.testval()
+        # Calculate sum of ranks for group A
+        n_1 = len(self.data1)
+        sum_ranks_1 = result[0] - (n_1 * (n_1 + 1)) / 2
+
+        # Calculate sum of ranks for group B
+        n_2 = len(self.data2)
+        sum_ranks_2 = (n_1 * n_2) - sum_ranks_1
+
+        # Print the sum of ranks and U statistic
+        print("Sum of ranks for group 1:", sum_ranks_1)
+        print("Sum of ranks for group 2:", sum_ranks_2)
+        print("U statistic:", result[0])
+
+if __name__ == '__main__':
+    data1 = [5, 7, 3, 8, 6]
+    data2 = [10, 12, 9, 11, 13]
+    mw = MannWhitney(data1, data2)
+    mw.steps()
+
+    data1 = [2.1, 4.0,5.4, 6.3, 8, 6]
+    data2 = [10, 12, 9, 11, 13]
+    mw1 = MannWhitney(data2, data1)
+    mw1.steps()
+            
+            
+  
