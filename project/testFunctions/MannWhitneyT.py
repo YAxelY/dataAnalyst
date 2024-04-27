a8373519549c3076482806184b8711e5119a97e1
YAxelY <mboumeu581@gmail.com>
2024-04-27 04:04:50 +0100

    changes

--- project/testFunctions/MannWhitneyT.py
+++ project/testFunctions/MannWhitneyT.py
@@ -13,10 +13,12 @@ if __name__ == '__main__':
     data2 = [10, 12, 9, 11, 13]
     mw = MannWhitney(data1, data2)
     mw.steps()
-
+    print("\n\n\n exemple du cahier")
     data1 = [2.1, 4.0, 6.3, 5.4, 4.8,3.7,6.1,3.3]
     data2 = [4.1, 0.6, 3.1, 2.5, 4.0 ,6.2 ,1.6 ,2.2 ,1.9, 5.4]
+    print(mw.formHyp())
     mw = MannWhitney(data2, data1)
     mw.steps()
+    mw.conclusion()
 
 
--- project/tests/MannWhitney.py
+++ project/tests/MannWhitney.py
@@ -1,4 +1,4 @@
-from scipy.stats import mannwhitneyu
+from scipy.stats import mannwhitneyu 
 
 class MannWhitney:
     def __init__(self, data1, data2,type="two-sided"):
@@ -8,7 +8,7 @@ class MannWhitney:
     def formHyp(self):
         
         
-        return "H_0: U_A = U_B \n H_1: U_A = U_B"
+        return "H_0: U_1 = U_2 \n H_1: U_1 = U_2"
     
     def distribution(self):
         # Add your Mann-Whitney U test p-value calculation here
@@ -25,21 +25,37 @@ class MannWhitney:
     
     def steps(self):
 
-        result=self.testval()
+        u1,p_value = self.testval()
         # Calculate sum of ranks for group A
         n_1 = len(self.data1)
-        sum_ranks_1 = result[0] - (n_1 * (n_1 + 1)) / 2
+        sum_ranks_1 = u1 + (n_1 * (n_1 + 1)) / 2
+
 
         # Calculate sum of ranks for group B
         n_2 = len(self.data2)
-        sum_ranks_2 = (n_1 * n_2) - sum_ranks_1
+        u2=n_1*n_2-u1
+        sum_ranks_2 = u2 + (n_2 * (n_2 + 1)) / 2
 
         # Print the sum of ranks and U statistic
         print("Sum of ranks for group 1:", sum_ranks_1)
+        print("U_1:", u1)
         print("Sum of ranks for group 2:", sum_ranks_2)
-        print("U statistic:", result[0])
+        print("U_2", u2)
+        print("p-value:", p_value )
+
+    def conclusion(self,alpha = 0.05):
+
+        _ , p_value = self.testval()
+        if p_value < alpha:
+            print("Reject the null hypothesis")
+        else:
+            print("Fail to reject the null hypothesis")
+
+       
+
 
 if __name__ == '__main__':
+
     data1 = [5, 7, 3, 8, 6]
     data2 = [10, 12, 9, 11, 13]
     mw = MannWhitney(data1, data2)
