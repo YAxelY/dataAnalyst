837912f8330d057e8de9d10ec8dc53c5b69acdd9
YAxelY <mboumeu581@gmail.com>
2024-04-29 08:40:38 +0100

    project clear out

--- project/data_file.py
+++
@@ -1,9 +0,0 @@
-
-import numpy as np
-data = [
-    np.array([5, 4, 8, 6, 3]),
-    np.array([9, 7, 8, 6, 9]),
-    np.array([3, 5, 2, 3, 7]),
-    np.array([2, 3, 4, 1, 4]),
-    np.array([7, 6, 9, 4, 7]),
-]
